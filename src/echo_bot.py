a5994cd6a4bd04fb1bf7cfab55e58e57835346b0
Ivan Kolominsky <ifkolominskiy@edu.hse.ru>
2023-10-19 17:11:21 +0300

    Functionality is reduced to responding to images

--- src/echo_bot.py
+++ src/echo_bot.py
@@ -23,46 +23,35 @@ class EchoBot:
     def run_long_pool(self) -> None:
         for event in self.long_poll.listen():
             if event.type == VkBotEventType.MESSAGE_NEW:
+                sender_id = event.raw['object']['message']['from_id']
+
+                if ('reply_message' in event.raw['object']['message'] or
+                        len(event.raw['object']['message']['fwd_messages']) != 0 or
+                        {attachment['type'] for attachment in event.raw['object']['message']['attachments']} != {
+                            'photo'} or
+                        len(event.raw['object']['message']['text']) != 0):
+                    self.session.method("messages.send", {
+                        'peer_id': sender_id,
+                        'message': 'Я умею отвечать только на картинки',
+                        'random_id': 0
+                    })
+
+                    continue
+
                 attachments = []
 
                 if event.raw['object']['message']['attachments']:
                     for attachment in event.raw['object']['message']['attachments']:
                         attachment_type = attachment['type']
 
-                        match attachment_type:
-                            case 'photo':
-                                attachment_id = attachment[attachment_type]['id']
-                                owner_id = attachment[attachment_type]['owner_id']
+                        attachment_id = attachment[attachment_type]['id']
+                        owner_id = attachment[attachment_type]['owner_id']
+                        access_key = attachment[attachment_type]['access_key']
 
-                                access_key = attachment[attachment_type]['access_key']
+                        attachments.append(f"{attachment_type}{owner_id}_{attachment_id}_{access_key}")
 
-                                attachments.append(f"{attachment_type}{owner_id}_{attachment_id}_{access_key}")
-
-                            case 'audio' | 'video':
-                                attachment_id = attachment[attachment_type]['id']
-                                owner_id = attachment[attachment_type]['owner_id']
-
-                                attachments.append(f"{attachment_type}{owner_id}_{attachment_id}")
-
-                            case 'link':
-                                attachments.append(f"{attachment[attachment_type]['url']}")
-
-                sender_id = event.raw['object']['message']['from_id']
-
-                message_text = event.raw['object']['message']['text']
-
-                send_values = {
+                self.session.method("messages.send", {
                     'peer_id': sender_id,
-                    'message': message_text if message_text else '',
                     'attachment': ','.join(attachments),
                     'random_id': 0
-                }
-
-                if 'reply_message' in event.raw['object']['message']:
-                    send_values['reply_to'] = event.raw['object']['message']['reply_message']['id']
-
-                if event.raw['object']['message']['fwd_messages']:
-                    send_values['forward_messages'] = ','.join(
-                        [str(message_data['id']) for message_data in event.raw['object']['message']['fwd_messages']])
-
-                self.session.method("messages.send", send_values)
+                })
